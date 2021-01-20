3586f7cc65cdde3cc3fad8d97ef05ea0765f37ba
Gesiere Deyabofa Tarasele <taraselegesiere@gmail.com>
2021-01-20 23:41:39 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,38 +1,23 @@
 const form = document.querySelector('.form');
-const input = document.querySelectorAll('.input');
-const btn = document.querySelector('.btn');
+const inputFields = document.querySelectorAll('.input');
 const formCard = document.querySelector('.form-card');
 const errorMessage = document.querySelectorAll('.error-message');
-const errorIcon = document.querySelectorAll('.error-icon');
-
-
-function validEmail(email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-}
 
 
 form.addEventListener('submit', (e) => {
-
-     input.forEach((inputs, index) => {
-         if (inputs.value === '' || inputs.value === null) {
-            e.preventDefault()
-            errorFunc(inputs,index)
-         }
-         
-         if (!validEmail(inputs)) {
-            errorFunc(inputs,index)
-         }
+    inputFields.forEach((input, index) => {
+        if (input.value.trim() === '' || input.value === null) {
+            e.preventDefault();
+            errorFunc(input, index)  
+        }          
     })
-    
-    
     
 });
 
 
-
-
-function errorFunc (inputs, index){
-    input[index].classList.add('input-warning');
+function errorFunc (input, index){
+    inputFields[index].classList.add('input-warning');
+    inputFields[index].classList.add('error-icon')
     formCard.classList.add('visible');
     errorMessage[index].classList.add('invalid');
 }
